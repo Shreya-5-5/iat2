e873a3d049ad9be536e0b87c52e5d2be6c9e5a42
Shreya-5-5 <shsp22ise@cmrit.ac.in>
2025-05-18 11:45:04 +0530

    azure Pipeline

---
+++ Javateam/src/main/java/com/shreyajava/App.java
@@ -0,0 +1,13 @@
+package com.shreyajava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
